9477814a53c5eb7b54ae9442365494c6df520761
unknown <henockdinberu11@gmail.com>
2023-07-10 23:55:30 -0400

    added setup for different file types

--- src/main.rs
+++ src/main.rs
@@ -23,38 +23,53 @@ fn main() {
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>>{
+
     for path in config.file_dir.into_iter()
     .filter(|x| x.is_ok())
     .map(|x| x.unwrap().path())
     .filter(|r| r.is_file()) {
             println!("Checking: {:?}", path);
-            
-            let mut t: String = match check_type(&path){
 
+            let t: String = check_type(&path).parse().unwrap();
+
+            println!("Inputting {:}", t);
+            
+            match t.as_str(){
                 "docx" => {
                     let mut file = Docx::open(path).expect("Cannot open file. ::");
                     let mut buf = String::new();
                     let _ = file.read_to_string(&mut buf);
-                    buf
+                    println!("Opening DOCX ::");
+                    println!("{buf}\n");
                 },
-                _ => {
-                    let content: String = fs::read_to_string(path)?; 
-                    let result: Vec<&str> = search(&config.query, &content);
-                    for lines in result{
+
+                "txt" => {
+                    let content = fs::read_to_string(path).unwrap();
+                    let res = search(&config.query, &content);
+
+                    println!("Opening TXT ::");
+                    for lines in res{
                         println!("{lines}");
                     }
-                    content
+                    println!("");
+                },
+
+                _ => {
+                    println!("Not openable ::\n");
                 },
             };
     }
 
+
+
     Ok(())
 }
 
-fn check_type<'a>(path: &PathBuf) -> &'a str{
-    let hold: &str = &path.display().to_string();
-    let ret: Vec<&str> = hold.split(".").collect();
-    return ret.get(1).unwrap().copy();
+fn check_type(path: &PathBuf) -> String{
+    let hold: String = String::from(path.display().to_string());
+    let ret: &Vec<&str> = &hold.split(".").collect();
+    print!("Type Check: {:?}\n", ret);
+    return ret.get(2).unwrap().to_string();
 }
 
 fn search<'a>(needle: &String, content: &'a String) -> Vec<&'a str>{
